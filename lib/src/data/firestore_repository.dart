489581b1f53197098bbdf1065526440be1805131
Naod <naodat.dev@gmail.com>
2024-05-02 12:18:14 +0300

    code refactored to remove uid

--- lib/src/data/firestore_repository.dart
+++ lib/src/data/firestore_repository.dart
@@ -8,7 +8,6 @@ class FirestoreRepository {
 
   Future<void> addJob(String uid, String title, String company) =>
       _firestore.collection('users/$uid/jobs').add({
-        'uid': uid,
         'title': title,
         'company': company,
       });
@@ -16,7 +15,6 @@ class FirestoreRepository {
   Future<void> updateJob(
           String uid, String jobId, String title, String company) =>
       _firestore.doc('users/$uid/jobs/$jobId').update({
-        'uid': uid,
         'title': title,
         'company': company,
       });
@@ -24,13 +22,10 @@ class FirestoreRepository {
       _firestore.doc('users/$uid/jobs/$jobId').delete();
 
   Query<Job> jobsQuery(String uid) {
-    return _firestore
-        .collection('users/$uid/jobs')
-        .withConverter(
+    return _firestore.collection('users/$uid/jobs').withConverter(
           fromFirestore: (snapshot, _) => Job.fromMap(snapshot.data()!),
           toFirestore: (job, _) => job.toMap(),
-        )
-        .where('uid', isEqualTo: uid);
+        );
   }
 }
 
