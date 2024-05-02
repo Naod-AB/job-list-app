5b7ce709fdac2a662982a462b25afb8d866aadee
Naod <naodat.dev@gmail.com>
2024-05-02 11:10:20 +0300

    implemented retrieveing data from firestore

--- lib/src/data/firestore_repository.dart
+++ lib/src/data/firestore_repository.dart
@@ -7,12 +7,16 @@ class FirestoreRepository {
   final FirebaseFirestore _firestore;
 
   Future<void> addJob(String uid, String title, String company) async {
-    final docRef = await _firestore.collection('Jobs').add({
+    final docRef = await _firestore.collection('jobs').add({
       'uid': uid,
       'title': title,
       'company': company,
     });
     debugPrint(docRef.id);
+  }
+
+  Query<Map<String, dynamic>> jobsQuery() {
+    return _firestore.collection('jobs');
   }
 }
 
--- lib/src/screens/home_screen.dart
+++ lib/src/screens/home_screen.dart
@@ -1,6 +1,8 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:faker/faker.dart';
 import 'package:faker_app_flutter_firebase/src/data/firestore_repository.dart';
 import 'package:faker_app_flutter_firebase/src/routing/app_router.dart';
+import 'package:firebase_ui_firestore/firebase_ui_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
@@ -11,12 +13,13 @@ class HomeScreen extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
-      appBar: AppBar(title: const Text('My Jobs'), actions: [
+      appBar: AppBar(title: const Text('My jobs'), actions: [
         IconButton(
           icon: const Icon(Icons.person),
           onPressed: () => context.goNamed(AppRoute.profile.name),
         )
       ]),
+      body: const JobsListView(),
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.add),
         onPressed: () {
@@ -31,6 +34,26 @@ class HomeScreen extends ConsumerWidget {
               );
         },
       ),
+    );
+  }
+}
+
+class JobsListView extends ConsumerWidget {
+  const JobsListView({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final firestoreRepository =
+        ref.watch(firestoreRepositoryProvider).jobsQuery();
+    return FirestoreListView(
+      query: firestoreRepository,
+      itemBuilder: (BuildContext context,
+          QueryDocumentSnapshot<Map<String, dynamic>> doc) {
+        return ListTile(
+          title: Text(doc['title']),
+          subtitle: Text(doc['company']),
+        );
+      },
     );
   }
 }
